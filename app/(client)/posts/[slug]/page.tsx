8ce9fb4d9dc3efaba17e79a2a194a8db73ab0c2b
Francisco Rocha <frankrochapineda@hotmail.com>
2024-08-09 16:40:07 -0600

    adjust pdf view for mobile devices

--- app/(client)/posts/[slug]/page.tsx
+++ app/(client)/posts/[slug]/page.tsx
@@ -12,13 +12,13 @@ import Navbar from "@/app/components/Navbar";
 const date = VT323({ weight: "400", subsets: ["latin"] });
 
 interface Params {
-  params: {
-    slug: string;
-  };
+	params: {
+		slug: string;
+	};
 }
 
 async function getPost(slug: string) {
-  const query = `
+	const query = `
     *[_type == "post" && slug.current == "${slug}"][0] {
       _id,
       title,
@@ -36,91 +36,105 @@ async function getPost(slug: string) {
       "chordsURL": chords.asset->url,
       "bothURL": bothPDF.asset->url,
   }`;
-  const post = await client.fetch(query);
-  return post;
+	const post = await client.fetch(query);
+	return post;
 }
 
 export const revalidate = 60;
 
 const Page = async ({ params }: Params) => {
-  const post: Post = await getPost(params?.slug);
+	const post: Post = await getPost(params?.slug);
 
-  if (!post) {
-    notFound();
-  }
+	if (!post) {
+		notFound();
+	}
 
-  const pdfFiles = [
-    { url: post?.lyricsURL, title: "Lyrics PDF" },
-    { url: post?.chordsURL, title: "Chords PDF" },
-    { url: post?.bothURL, title: "Chords and Lyrics PDF" }
-  ].filter(pdf => pdf.url);
+	const pdfFiles = [
+		{ url: post?.lyricsURL, title: "Lyrics PDF" },
+		{ url: post?.chordsURL, title: "Chords PDF" },
+		{ url: post?.bothURL, title: "Chords and Lyrics PDF" },
+	].filter((pdf) => pdf.url);
 
-  return (
-    <div>
-      <Navbar title={post?.title} author={post?.author} />
-      <div className="text-center">
-        <span className={`${date.className}`}>
-          {(() => {
-            const formattedDate = new Date(post?.publishDate).toLocaleDateString("es-ES", {
-              weekday: "long",
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            });
-            return (
-              formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1)
-            );
-          })()}
-        </span>
-        <div className="mt-5">
-          {post?.tags?.map((tag) => (
-            <Link key={tag?._id} href={`/tag/${tag?.slug?.current}`}>
-              <span className="mr-2 p-1 text-gray-500 rounded-sm lowercase dark:border-gray-900">
-                #{tag?.name}
-              </span>
-            </Link>
-          ))}
-        </div>
-        <div className={richTextStyles}>
-          <PortableText
-            value={post?.body}
-            components={myPortableTextComponents}
-          />
-        </div>
+	return (
+		<div>
+			<Navbar
+				title={post?.title}
+				author={post?.author}
+			/>
+			<div className="text-center">
+				<span className={`${date.className}`}>
+					{(() => {
+						const formattedDate = new Date(
+							post?.publishDate
+						).toLocaleDateString("es-ES", {
+							weekday: "long",
+							year: "numeric",
+							month: "long",
+							day: "numeric",
+						});
+						return (
+							formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1)
+						);
+					})()}
+				</span>
+				<div className="mt-5">
+					{post?.tags?.map((tag) => (
+						<Link
+							key={tag?._id}
+							href={`/tag/${tag?.slug?.current}`}
+						>
+							<span className="mr-2 p-1 text-gray-500 rounded-sm lowercase dark:border-gray-900">
+								#{tag?.name}
+							</span>
+						</Link>
+					))}
+				</div>
+				<div className={richTextStyles}>
+					<PortableText
+						value={post?.body}
+						components={myPortableTextComponents}
+					/>
+				</div>
 
-        {/* Sección deslizable para los PDFs */}
-        <div className="mt-10 overflow-x-auto">
-          <div className="flex space-x-4">
-            {pdfFiles.map((pdf, index) => (
-              <div key={index} className="flex-shrink-0 lg:w-[33vw] md:w-[50vw] sm:w-[100vw]">
-                <h3 className="text-xl font-bold mb-4">{pdf.title}</h3>
-                <iframe
-                  src={pdf.url}
-                  width="100%"
-                  height="600px"
-                  className="border-0"
-                ></iframe>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+				{/* Sección deslizable para los PDFs */}
+				<div className=" mt-10 mb-4 overflow-x-auto min-h-[35vw] ">
+					<div className="flex space-x-2 md:space-x-4 max-w-full min-h-[30vw] justify-center">
+						{pdfFiles.map((pdf, index) => (
+							<div
+								key={index}
+								className=" w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
+							>
+								<h3 className="text-lg md:text-xl font-bold mb-2 md:mb-4">
+									{pdf.title}
+								</h3>
+								<iframe
+									src={pdf.url}
+									width="100%"
+									height="100%" // Ajuste adicional para la altura en móviles
+									className="border-0"
+								></iframe>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Page;
 
 const myPortableTextComponents = {
-  types: {
-    image: ({ value }: any) =>
-      <Image
-        src={urlFor(value).url()}
-        alt="Post"
-        width={700}
-        height={700}
-      />,
-  },
+	types: {
+		image: ({ value }: any) => (
+			<Image
+				src={urlFor(value).url()}
+				alt="Post"
+				width={700}
+				height={700}
+			/>
+		),
+	},
 };
 
 const richTextStyles = `
